ed9bf832d140fa91d386ec725564d6ae797d1674
AdamHoppe <42608473+agsdkglf@users.noreply.github.com>
2023-03-24 21:16:48 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# Portfolio--GUI-Image-Watermarking-Desktop-App
+# Image Watermarking Desktop App
+The project uses the Python Pillow library to put a watermark on one of the given images. The Graphical User Interface is created with the use of Tkinter.
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Screenshots](#screenshots)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Acknowledgements](#acknowledgements)
+* [Contact](#contact)
+* [License](#license)
+
+
+## General Information
+The project allows the user to put a watermark on a given picture. After running the code, the iterface appears showing an option to choose one of pictures. The chosen picture receives a watermark that is put on it.
+
+
+## Technologies Used
+- Pillow 9.4.0
+
+
+## Screenshots
+![Example screenshot number 1](./img/screenshot1.png)
+![Example screenshot number 2](./img/screenshot2.png)
+
+
+## Setup
+The project's requirements are listed in a "requirements.txt" file.
+
+Download the code from this GitHub repository, two example images ("sunset", "test_sink") and activate the virtual environment. Make sure that you have Python 3.6 or newer installed. The next step is to install the Pillow using pip within a virtual environment:
+
+    PS> python -m venv venv
+
+    PS> .\venv\Scripts\activate
+
+    (venv) PS> python -m pip install Pillow
+
+Next, you are ready to run the code. When running the code, you get the following error:
+
+    FileNotFoundError: [Errno 2] No such file or directory: 'logo.png'
+
+you should use an absolute path instead of a relative one, in the lines:
+
+    watermark = "logo.png"
+
+    sunset = "sunset.png"
+
+    sink = "test_sink.png"
+
+## Usage
+After fulfilling the instructions described in "Setup" you are ready to use the code.
+
+
+## Project Status
+The project's status is _complete_.
+
+
+## Room for Improvement
+As for future development it would be worth to offer the users a possibility to put their own watermarks on their own images.
+
+
+## Acknowledgements
+This project was based on "Image Processing With the Python Pillow Library" (https://realpython.com/image-processing-with-the-python-pillow-library/).
+
+
+## Contact
+Created by Adam Hoppe (adhoppe@poczta.fm) - feel free to contact me!
+
+
+## License
+This project is open source and available under the MIT License.
--- main.py
+++ main.py
@@ -2,52 +2,49 @@ import tkinter as tk
 from tkinter import font
 from PIL import Image, ImageFilter
 
-# Get the watermark image.
-watermark = 'C:/Users/user/Documents/Computer programming/Python/Portfolio Projects/4 [GUI] Image Watermarking ' \
-            'Desktop App/logo.png '
+# Get the watermark image
+watermark = "logo.png"
 
 with Image.open(watermark) as img_watermark:
     img_watermark.load()
 
 img_watermark = Image.open(watermark)
 
-# Change the watermark image to grayscale and threshold it.
+# Change the watermark image to grayscale and threshold it
 img_watermark = img_watermark.convert("L")
 threshold = 50
 img_watermark = img_watermark.point(lambda x: 255 if x > threshold else 0)
 
-# Reduce its size and transform it into a contour image.
+# Reduce its size and transform it into a contour image
 img_watermark = img_watermark.resize((img_watermark.width // 2, img_watermark.height // 2))
 img_watermark = img_watermark.filter(ImageFilter.CONTOUR)
 
-# Reverse the colors.
+# Reverse the colors
 img_watermark = img_watermark.point(lambda x: 0 if x == 255 else 255)
 
 
 def image_1():
-    sunset = 'C:/Users/user/Documents/Computer programming/Python/Portfolio Projects/4 [GUI] Image Watermarking ' \
-             'Desktop App/sunset.png '
+    sunset = "sunset.png"
 
     with Image.open(sunset) as img_sunset:
         img_sunset.load()
 
     img_sunset = Image.open(sunset)
 
-    # Paste img_watermark onto img_sunset.
+    # Paste img_watermark onto img_sunset
     img_sunset.paste(img_watermark, (3700, 2800), img_watermark)
     img_sunset.show()
 
 
 def image_2():
-    sink = 'C:/Users/user/Documents/Computer programming/Python/Portfolio Projects/4 [GUI] Image Watermarking Desktop ' \
-           'App/test_sink.png '
+    sink = "test_sink.png"
 
     with Image.open(sink) as img_sink:
         img_sink.load()
 
     img_sink = Image.open(sink)
 
-    # Paste img_watermark onto img_sunset.
+    # Paste img_watermark onto img_sunset
     img_sink.paste(img_watermark, (2600, 3800), img_watermark)
     img_sink.show()
 
@@ -58,9 +55,9 @@ window.title('Add a watermark to a picture')
 labelFont = font.Font(family='Helvetica', name='appHighlightFont', slant='italic', size=17, weight='bold')
 buttonFont = font.Font(family='Helvetica', underline=1, size=16, weight='bold')
 
-# Configure row INDEX of a grid.
+# Configure row INDEX of a grid
 window.rowconfigure(0, minsize=100, weight=1)
-# Configure column INDEX of a grid.
+# Configure column INDEX of a grid
 window.columnconfigure([0, 1, 2], minsize=100, weight=1)
 
 btn_image_1 = tk.Button(master=window, font=buttonFont, text='Choose picture number 1', command=image_1)
